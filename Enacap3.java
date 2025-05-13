b688f9f935376dfc984aefc2876d320df3147bc2
Joyal <joyaldaniel125@gmail.com>
2025-05-13 13:26:23 +0530

    Add files via upload

--- Enacap3.java
+++ Enacap3.java
@@ -1,7 +1,32 @@
+class Rectangle1{
 
+    private int length;
+    private int width;
+
+    public int getWidth() {
+        return length*width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getLength() {
+        return 2*(length+width);
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+}
 
 public class Enacap3 {
     public static void main(String[]args){
+Rectangle1 s1 = new Rectangle1();
 
+s1.setWidth(32);
+s1.setLength(54);
+System.out.println(s1.getLength());
+System.out.println(s1.getWidth());
     }
 }
